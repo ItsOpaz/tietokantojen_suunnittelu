92825afe694fb67e4c12e9774971a97a3061b748
Joonas Kakkuri <joonas.kakkuri@gmail.com>
2020-01-10 21:05:29 +0200

    Lisäätty funktio, joka lisää uusia käyttäjiä sekä yritin lisätä koko tietokannan. Sitä en tiedä toimiiko koko roska ollenkaan, löytyy data-kansiosta.

--- iflack.sql
+++ iflack.sql
@@ -5,7 +5,7 @@
 -- Eli pitääkö esim tähän laittaa rooli? tms.
 CREATE TABLE jarjestelma_kayttaja (
 	
-	kayttaja_tunnus SERIAL PRIMARY KEY,
+	kayttaja_tunnus VARCHAR(30) PRIMARY KEY,
 	etunimi VARCHAR NOT NULL,
 	sukunimi VARCHAR NOT NULL,
 	tila BOOLEAN
@@ -15,20 +15,86 @@ CREATE TABLE jarjestelma_kayttaja (
 -- Kirjautumistiedot
 CREATE TABLE jarjestelma_kirjautumistiedot (
 	
-	kayttaja_tunnus int PRIMARY KEY,
+	kayttaja_tunnus VARCHAR(30) PRIMARY KEY,
 	salasana VARCHAR NOT NULL,
-	 FOREIGN KEY(kayttaja_tunnus) REFERENCES jarjestelma_kayttaja(kayttaja_tunnus)
+	FOREIGN KEY(kayttaja_tunnus) REFERENCES jarjestelma_kayttaja(kayttaja_tunnus)
+	ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Testi dataa
-INSERT INTO jarjestelma_kayttaja(etunimi, sukunimi, tila) VALUES(
-
+INSERT INTO jarjestelma_kayttaja VALUES(
+	'isoTony69',
 	'Toni',
 	'Halme',
 	false);
 
-INSERT INTO jarjestelma_kayttaja(etunimi, sukunimi, tila) VALUES(
-
+INSERT INTO jarjestelma_kayttaja VALUES(
+	'MenkaaToihin',
 	'Donald',
 	'Trump',
 	false);
+
+
+INSERT INTO jarjestelma_kirjautumistiedot (kayttaja_tunnus, salasana) VALUES(
+
+		'MenkaaToihin',
+		'BuildAWall'
+);
+
+INSERT INTO jarjestelma_kirjautumistiedot (kayttaja_tunnus, salasana) VALUES(
+
+		'isoTony69',
+		'painuPelleHiiteen'
+);
+
+
+-- Lisää uuden käyttäjän järjestelmään
+-- EI VIELÄ MITÄÄN VIRHETARKASTELUA!!! 
+
+-- first_name, last_name, user_name, password
+CREATE FUNCTION add_user(VARCHAR, VARCHAR, VARCHAR(30), VARCHAR) RETURNS void AS $$
+	
+	WITH ins1 AS (
+		INSERT INTO jarjestelma_kayttaja VALUES(
+		
+		$3,
+		$1,
+		$2,
+		false
+	)
+	
+	WITH ins2 AS (
+		INSERT INTO jarjestelma_kirjautumistiedot VALUES(
+			$3,
+			$4
+	)
+	
+$$ LANGUAGE SQL;
+
+
+CREATE OR REPLACE FUNCTION add_user(VARCHAR, VARCHAR, VARCHAR(30), VARCHAR) 
+	RETURNS boolean AS 
+	$func$
+	
+	BEGIN
+		INSERT INTO jarjestelma_kayttaja VALUES(
+			
+			$3,
+			$1,
+			$2,
+			false
+		);
+
+		IF NOT FOUND THEN
+			RETURN FALSE;
+		END IF;
+
+		INSERT INTO jarjestelma_kirjautumistiedot VALUES(
+			$3,
+			$4
+		);
+
+		RETURN FOUND;
+	END
+	$func$ LANGUAGE plpgsql;
+
