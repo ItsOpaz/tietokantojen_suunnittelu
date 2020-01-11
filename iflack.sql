47195055abeefa85b75bb84f87f33aac14cbc2e4
Joonas Kakkuri <joonas.kakkuri@gmail.com>
2020-01-11 19:34:10 +0200

    lisätty profiili, mainoskampanja, laskurivi, lasku sekä vähän testidataa

--- iflack.sql
+++ iflack.sql
@@ -1,4 +1,10 @@
 
+/*
+	En oo laittanu vielä esim NOT NULL lisämääreitä iha hirvesti
+	Koitan vasta saada jonkunäkösen kannan pystyyn ja kattoo mite toimii
+	lisää sit noi virhetarkastelut myöhemmin
+*/
+
 -- Järjestelmän käyttäjä
 
 -- Pitääköhän mainosmyyjä ja sihteeri erotella jotenki?
@@ -49,7 +55,7 @@ INSERT INTO jarjestelma_kirjautumistiedot (kayttaja_tunnus, salasana) VALUES(
 
 
 -- Lisää uuden käyttäjän järjestelmään
--- EI VIELÄ MITÄÄN VIRHETARKASTELUA!!! 
+-- Pitää kai lisätä virhetarkastelu vai voiko sen tehdä koodin puolella
 
 -- first_name, last_name, user_name, password
 CREATE OR REPLACE FUNCTION add_user(VARCHAR, VARCHAR, VARCHAR(30), VARCHAR) 
@@ -116,6 +122,9 @@ INSERT INTO yhteyshenkilo(etunimi, sukunimi, email, puhelinnumero) VALUES(
 	'Pekka', 'Penttilä', 'pekupena@tuni.fi', '6969696969'
 );
 
+-- Eikös tässä voi olla tilanne, jossa mainostajalla ei ole yhteyshenkilöä?
+-- Jos poistetaan yhteyshenkilö, tilalle jää null- arvo
+-- oliko tää sallittua
 CREATE TABLE mainostaja(
 	vat VARCHAR(30) PRIMARY KEY,
 	nimi VARCHAR(30),
@@ -126,9 +135,111 @@ CREATE TABLE mainostaja(
 );
 
 INSERT INTO mainostaja VALUES(
-	'45 TUNIPATSAS',
-	'Mainostoimisto Masa',
+	'45 TUNIPATSAS', -- vat
+	'Mainostoimisto Masa', -- nimi
+	1, -- yht. hlö. id
+	1 -- laskutusosoite id
+);
+
+
+-- Experimental
+
+CREATE TABLE lasku(
+
+	laskuId SERIAL PRIMARY KEY,
+	lahetyspvm DATE,
+	eraPvm DATE,
+	tila boolean,
+	viitenro VARCHAR(20),
+	korko decimal(5,2),
+	riviId integer -- Ei aseteta vierasavainta vielä
+);
+
+CREATE TABLE profiili(
+
+	profiiliId SERIAL PRIMARY Key,
+	lahetysAika TIME,
+	maa VARCHAR(20),
+	paikkakunta VARCHAR(40),
+	alaikaraja integer,
+	CHECK(alaikaraja > 0),
+	ylaikaraja integer,
+	CHECK(ylaikaraja > 0)
+);
+
+-- Tämä ei tarkista vielä sitä xor- suhdetta profiilin ja mainoksen välillä
+CREATE TABLE mainoskampanja(
+	kampanjaId SERIAL PRIMARY KEY,
+	laskuId integer,
+	FOREIGN Key(laskuId) REFERENCES lasku(laskuId)
+	ON UPDATE CASCADE ON DELETE SET NULL,
+
+	nimi VARCHAR(40),
+	alkupvm DATE DEFAULT CURRENT_DATE,
+	loppupvm DATE,
+	maaraRahat MONEY,
+	sekuntihinta MONEY,
+	tila boolean DEFAULT false NOT NULL, -- enabled/disabled
+
+	profiiliId integer,
+	FOREIGN Key(profiiliId) REFERENCES profiili(profiiliId)
+	ON UPDATE CASCADE ON DELETE SET NULL
+
+);
+
+CREATE TABLE laskurivi(
+
+	riviId SERIAL PRIMARY KEY,
+	selite VARCHAR(40),
+	hinta MONEY,
+	kampanjaId integer,
+	FOREIGN KEY(kampanjaId) REFERENCES mainoskampanja(kampanjaId)
+	ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+-- Muutetaan laskurivi vielä laskun vierasavaimeksi
+ALTER TABLE lasku
+	ADD CONSTRAINT fk_lasku_rivi FOREIGN KEY (riviId) REFERENCES laskurivi(riviId)
+	ON DELETE SET NULL ON UPDATE CASCADE;
+
+INSERT INTO lasku (	lahetyspvm,
+					eraPvm,
+					tila,
+					viitenro,
+					korko,
+					riviId) VALUES(
+						
+						'2020-11-1',
+						'2020-11-2',
+						false,
+						'123452346',
+						12.00,
+						NULL
+
+					);
+
+INSERT INTO profiili VALUES (
 	1,
+	'00:00',
+	'Suomi',
+	'Tampere',
+	3,
+	null
+);
+
+INSERT INTO mainoskampanja (laskuId, nimi, loppupvm, maaraRahat, sekuntihinta, tila, profiiliId) VALUES(
+
+	1,
+	'masan mainoskampanja',
+	'2020-08-30',
+	100.00,
+	0.20,
+	false,
 	1
 );
 
+INSERT INTO laskurivi(selite, hinta, kampanjaId) VALUES (
+	'Perkeleen kallis mainos',
+	99.99,
+	1
+);
