a4cf956978ecdde979b74aa17cde0293081cb998
Santeri Nuotiomaa <nuotioms@linux-desktop9.tuni.fi>
2020-01-28 11:10:01 +0200

    luotu populoinnit osoitteille ja yhteyshenkl

---
+++ mainostajaosoitta.sql
@@ -0,0 +1,50 @@
+SELECT lisaa_laskutusosoite('Hermiankatu 5', '33720', 'Tampere', 'Suomi');
+INSERT INTO yhteyshenkilo(etunimi, sukunimi, email, puhelinnumero)
+
+	VALUES(
+    
+	 'Masa',
+   
+	 'Meikalainen',
+    
+	 'masa69@gmail.com',
+   
+ 	 '0408679549'
+);
+
+
+SELECT lisaa_laskutusosoite('Hatanpään valtaväylä 13', '33900', 'Tampere', 'Suomi');
+INSERT INTO yhteyshenkilo(etunimi, sukunimi, email, puhelinnumero)
+
+	VALUES(
+    
+	 'Maiju',
+   
+	 'Mahdoton',
+    
+	 'maijudaa@gmail.com',
+   
+ 	 '0404983751'
+);
+
+
+SELECT lisaa_laskutusosoite('Satakunnankatu 14', '33210', 'Tampere', 'Suomi');
+INSERT INTO yhteyshenkilo(etunimi, sukunimi, email, puhelinnumero)
+
+	VALUES(
+    
+	 'Essi',
+   
+	 'Esimerkillinen',
+    
+	 'essi.esimerkillinen@gmail.com',
+   
+ 	 '0405927186'
+);
+-- Masa 7
+-- Maiju 8
+-- Essi 9
+-- Hermia 6
+-- Hatanpää 7
+-- Satakunta 8
+
