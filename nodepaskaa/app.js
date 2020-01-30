1da19a48fe139f6b88ed8e36183e7b0ac5fcf63d
Santeri Nuotiomaa <santeri.nuotiomaa@tuni.fi>
2020-01-30 22:10:28 +0200

    Update app.js

--- nodepaskaa/app.js
+++ nodepaskaa/app.js
@@ -6,7 +6,8 @@ var client = new pg.Client(conString);
 client.connect();
 client.query(('SELECT * FROM mainos'), function (err, result, fields) {
     if (err) throw err;
-    console.log(result);
+    console.log(result.rows);
+    
   });
 app.get( '/' ,(req, res) => {
   res.sendFile(__dirname+'/views/home.html');
