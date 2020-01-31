1e765ed41f3f5b3046835af8f924435273414a56
Joonas Kakkuri <joonas.kakkuri@gmail.com>
2020-01-31 16:47:54 +0200

    lisätty tarvittavat sivut, login sivun raaka ui tehty

--- nodepaskaa/iflac/src/Login.js
+++ nodepaskaa/iflac/src/Login.js
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Redirect } from 'react-router-dom'
+
+
+
 
 const Login = (props) => {
+    const [redirect, setRedirect] = useState(false)
+    const [errmsg, setErrmsg] = useState("")
 
+    const [kayttajatunnus, setKayttajatunnus] = useState("")
+    const [salasana, setSalasana] = useState("")
+
+    const handleSignIn = (event) => {
+        event.preventDefault()
+        setErrmsg("JOkin meni vikaan...")
+        console.log(event)
+        setRedirect(true)
+    }
+
+    const renderRedirect = () => {
+        if (redirect) {
+            return <Redirect to='/' />
+        }
+    }
 
     return (
-        <p>
-            Login
-        </p>
+        <div className="login">
+            <h3>IFLAC</h3>
+
+            <h5>Kirjaudu</h5>
+
+            <form onSubmit={handleSignIn}>
+
+                <input type="text" name={kayttajatunnus} placeholder="Käyttäjätunnus" />
+                <input type="text" name={salasana} placeholder="Salasana" />
+                <input type="submit" value="Kirjaudu"></input>
+            </form>
+
+            <h4>{errmsg}</h4>
+            {renderRedirect()}
+
+        </div>
     )
 }
 
