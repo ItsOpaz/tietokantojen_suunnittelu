1de4ca09506af0def66547d855b97858688c8733
Santeri Nuotiomaa <santeri.nuotiomaa@tuni.fi>
2020-02-01 16:32:20 +0200

    testistä

--- nodepaskaa/app.js
+++ nodepaskaa/app.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
 const pg = require('pg');
-var conString = "postgres://sqlmanager:keittovesa@localhost:5432/iflac";
+var conString = "postgres://postgres:130m3ga11@localhost:5432/tika";
 var client = new pg.Client(conString);
 client.connect();
 
 app.get( '/' ,(req, res) => {
    client.query(('SELECT * FROM mainos'), function (err, result, fields) {
     if (err) throw err;
+    var data = result.rows;
     console.log(result.rows);
-    res.write(JSON.stringify(result.rows));
+    res.sendFile(__dirname+'/views/home.html', {data});
   });
   //res.sendFile(__dirname+'/views/home.html')
-  
+
 });
 
 
